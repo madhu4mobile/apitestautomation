9f4824178a33173f873d79b99a9c6b8e4062fdce
Madhu Muppala <madhu4mobile@gmail.com>
2023-01-20 22:08:59 -0600

    initial commit

---
+++ src/main/java/constants/EndPoints.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class EndPoints {
+    public static final String GET_USER = "/api/users/2";
+    public static final String REGISTER_USER = "/api/register";
+    public static final String UPDATE_USER = "/api/users/2";
+    public static  final String DELETE_USER ="/api/users/2";
+
+}
